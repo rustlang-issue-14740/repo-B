d35f10c5735ab7e253d20c5b105547f3a54efcdf
Lukas Rieger <code@lukasrieger.com>
2024-11-06 08:48:24 +0100

    add macro

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,16 @@
 // pub fn div(left: u64, right: u64) -> u64 {
 //     left / right
 // }
+
+use proc_macro::TokenStream;
+
+#[proc_macro_attribute]
+pub fn fake_macro(meta: TokenStream, input: TokenStream) -> TokenStream {
+
+    if repo_A::add(1, 1) == 2 {
+        return input;
+    } else {
+        return meta;
+    }
+
+}
