0ccca8524d0db25252dc139e8720a6d6d4a537c7
Lukas Rieger <code@lukasrieger.com>
2024-11-06 07:39:09 +0100

    git ref and use feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn mul(left: u64, right: u64) -> u64 {
+    let mut result = 0;
+    for _i in 0 .. left {
+        result = repo_A::add(result, right);
+    }
+    left * right
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn div(left: u64, right: u64) -> u64 {
+    left / right
 }
