52634f74086db0a7a4755bcd5d800242da3bedd5
Lukas Rieger <code@lukasrieger.com>
2024-11-06 08:36:25 +0100

    make proc macro crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-pub fn mul(left: u64, right: u64) -> u64 {
-    let mut result = 0;
-    for _i in 0 .. left {
-        result = repo_A::add(result, right);
-    }
-    left * right
-}
+// pub fn mul(left: u64, right: u64) -> u64 {
+//     let mut result = 0;
+//     for _i in 0 .. left {
+//         result = repo_A::add(result, right);
+//     }
+//     left * right
+// }
 
-pub fn div(left: u64, right: u64) -> u64 {
-    left / right
-}
+// pub fn div(left: u64, right: u64) -> u64 {
+//     left / right
+// }
